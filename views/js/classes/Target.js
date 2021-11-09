8becc682a8ea901ee33f6e72b23b1ebd69a59ce4
CptMog <djawad.mohamed8@gmail.com>
2021-11-09 13:17:09 +0100

    Adding differences bewtween targets

--- views/js/classes/Target.js
+++ views/js/classes/Target.js
@@ -1,7 +1,7 @@
 export default class Target{
-
+    
     constructor(objDom){
-
+        this.COLOR = { kind:'#4B2D4D', evil:'red' };
         this.objDom = objDom;
         this.score = 0;
         this.targets = new Array();
@@ -10,10 +10,10 @@ export default class Target{
         this.ctx.fillText('Click the start button',this.objDom.width/3.5, this.objDom.height/2);
     }
 
-    drawTarget(x,y){
+    drawTarget(x,y,color){
         
         this.ctx.beginPath();
-        this.ctx.fillStyle = '#4B2D4D';
+        this.ctx.fillStyle = color;
         this.ctx.arc(x, y, 60 , 0, 360, 0);
         this.ctx.fill();
         this.ctx.closePath();
@@ -35,7 +35,8 @@ export default class Target{
                 this.ctx.arc(coord.corx,coord.cory, 61 , 0, 360, 0);
                 this.ctx.fill();
                 this.ctx.closePath();
-                this.setScore = 100;
+
+                this.setScore = (coord.type == this.COLOR.evil? 100 : -100);
             }
 
             this.updateBoard()
@@ -47,6 +48,7 @@ export default class Target{
                 }
             })
 
+            console.log(this.targets);
             
         }
 
@@ -62,10 +64,11 @@ export default class Target{
         let curr_target = 1;
 
         while(curr_target <= nbr){
+            const color = ((Math.floor(Math.random() * 3)%2 == 0)?this.COLOR.evil:this.COLOR.kind);
             let x = Math.floor(Math.random() * this.objDom.width-50);
             let y = Math.floor(Math.random() * this.objDom.height+50);
-            setTimeout(() => { this.drawTarget(x,y) },curr_target*500);
-            this.targets.push({corx : x, cory : y});
+            setTimeout(() => { this.drawTarget(x,y,color) },curr_target*500);
+            this.targets.push({corx : x, cory : y, type:color});
             curr_target++;
         }
        
